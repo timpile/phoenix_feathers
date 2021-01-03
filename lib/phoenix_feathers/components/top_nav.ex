9b4cbd0e0c536a51ac16b6a95d9638cffc90e8f8
Tim Pile <timspile@gmail.com>
2021-01-03 12:42:31 -0500

    Add mobile responsiveness to navigation

--- lib/phoenix_feathers/components/top_nav.ex
+++ lib/phoenix_feathers/components/top_nav.ex
@@ -1,13 +1,16 @@
 defmodule PhoenixFeathers.TopNav do
   use PhoenixFeathers.LiveComponent
 
+  alias PhoenixFeathers.Icon
+
   @doc """
   Example
 
   ```
   <%= live_component @socket,
     PhoenixFeathers.TopNav,
-    home: %{url: "/", title: "Home", logo_src: "/path/to/logo.svg"},
+    id: "phoenix_feathers_top_nav",
+    brand: %{url: "/", title: "Brand", logo_src: "/path/to/logo.svg"},
     nav_items: [%{url: "/", text: "Link 1"}, %{url: "/", text: "Link 2"}]
   %>
   ```
@@ -16,13 +19,23 @@ defmodule PhoenixFeathers.TopNav do
   def render(assigns) do
     ~L"""
       <nav class="phx_feathers_top_nav">
-        <div class="left">
-          <a href="<%= @home.url %>">
-            <img src="<%= @home.logo_src %>">
-            <span class="title"><%= @home.title %></span>
+        <div class="brand">
+          <a href="<%= @brand.url %>">
+            <img src="<%= @brand.logo_src %>">
+            <span class="title"><%= @brand.title %></span>
           </a>
         </div>
-        <div class="right">
+        <div class="menu" data-is-mobile-open="<%= @is_mobile_open %>">
+          <div class="mobile_menu">
+            <a
+              href="#"
+              class="toggle_mobile_menu"
+              phx-click="toggle_mobile_menu"
+              phx-target="<%= @myself %>"
+            >
+              <%= toggle_icon(assigns, @is_mobile_open) %>
+            </a>
+          </div>
           <ul>
             <%= for item <- @nav_items do %>
               <li>
@@ -40,6 +53,17 @@ defmodule PhoenixFeathers.TopNav do
   end
 
   def update(assigns, socket) do
-    {:ok, socket |> assign(assigns)}
+    {:ok, socket |> assign(assigns) |> assign(is_mobile_open: false)}
   end
+
+  def handle_event(
+        "toggle_mobile_menu",
+        _params,
+        %{assigns: %{is_mobile_open: is_mobile_open}} = socket
+      ) do
+    {:noreply, socket |> assign(is_mobile_open: !is_mobile_open)}
+  end
+
+  defp toggle_icon(assigns, true), do: render(assigns, %Icon{name: :close, color: "#fff"})
+  defp toggle_icon(assigns, false), do: render(assigns, %Icon{name: :menu, color: "#fff"})
 end
--- lib/phoenix_feathers/icon.ex
+++ lib/phoenix_feathers/icon.ex
@@ -23,6 +23,12 @@ defmodule PhoenixFeathers.Icon do
     """
   end
 
+  def svg(assigns, %PhoenixFeathers.Icon{name: :close, color: color}) do
+    ~L"""
+    <svg viewBox="0 0 20 20"><path fill="<%= color %>" d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"/></svg>
+    """
+  end
+
   def svg(assigns, %PhoenixFeathers.Icon{name: :menu, color: color}) do
     ~L"""
     <svg viewBox="0 0 20 20"><path fill="<%= color %>" d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
